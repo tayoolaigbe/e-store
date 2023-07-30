28b8a8cc3015983725a4fb7a4c7801cd64b63319
tayoolaigbe <tayoolaigbe@outlook.com>
2023-07-30 17:18:26 +0100

    Added the ImageUpload component with cloudinary

--- ecommerce-admin/app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
+++ ecommerce-admin/app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
@@ -1,12 +1,15 @@
 'use client';
 
 import { Plus } from 'lucide-react';
+import { useParams, useRouter } from 'next/navigation';
 
 import { Button } from '@/components/ui/button';
 import { Heading } from '@/components/ui/heading';
 import { Separator } from '@/components/ui/separator';
 
 export const BillboardClient = () => {
+	const params = useParams();
+	const router = useRouter();
 	return (
 		<>
 			<div className="flex items-center justify-between">
@@ -14,7 +17,9 @@ export const BillboardClient = () => {
 					title="Billboards (0)"
 					description="Manage billboards for your store."
 				/>
-				<Button>
+				<Button
+					onClick={() => router.push(`/${params.storeId}/billboards/new`)}
+				>
 					<Plus className="mr-2 h-4 w-4" />
 					Add New
 				</Button>
