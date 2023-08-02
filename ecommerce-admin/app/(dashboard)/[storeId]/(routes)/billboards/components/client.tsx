1c93c217b3162932c2a20c72c49b4bf235c38fa5
tayoolaigbe <tayoolaigbe@outlook.com>
2023-08-02 22:30:58 +0100

    Added the ApiList componentand made the GET routes public in middleware

--- ecommerce-admin/app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
+++ ecommerce-admin/app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
@@ -8,6 +8,7 @@ import { Heading } from '@/components/ui/heading';
 import { Separator } from '@/components/ui/separator';
 import { BillboardColumn, columns } from './columns';
 import { DataTable } from '@/components/ui/data-table';
+import { ApiList } from '@/components/ui/api-list';
 
 interface BillboardProps {
 	data: BillboardColumn[];
@@ -32,6 +33,9 @@ export const BillboardClient: React.FC<BillboardProps> = ({ data }) => {
 			</div>
 			<Separator />
 			<DataTable searchKey="label" columns={columns} data={data} />
+			<Heading title="API" description="API calls for Billboards" />
+			<Separator />
+			<ApiList entityName="billboards" entityIdName="billboardId" />
 		</>
 	);
 };
