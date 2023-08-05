698ea2a19679134d5c01a04bbb80931ba5ea0649
tayoolaigbe <tayoolaigbe@outlook.com>
2023-08-05 23:04:47 +0100

    Started the ecommerce-store application with NextJs and added a simple footer.tsx

---
+++ ecommerce-store/app/layout.tsx
@@ -0,0 +1,26 @@
+import Footer from '@/components/footer';
+import './globals.css';
+import type { Metadata } from 'next';
+import { Urbanist } from 'next/font/google';
+
+const urbanist = Urbanist({ subsets: ['latin'] });
+
+export const metadata: Metadata = {
+	title: 'Store',
+	description: 'Store',
+};
+
+export default function RootLayout({
+	children,
+}: {
+	children: React.ReactNode;
+}) {
+	return (
+		<html lang="en">
+			<body className={urbanist.className}>
+				{children}
+				<Footer />
+			</body>
+		</html>
+	);
+}
