1fabef2b7b0ba0fe0b5706a24e19e6375d85586a
tayoolaigbe <tayoolaigbe@outlook.com>
2023-08-09 22:05:40 +0100

    Added the Billboard page to the frontend and created the get-billboard function to fetch the billboard

--- ecommerce-store/app/(routes)/page.tsx
+++ ecommerce-store/app/(routes)/page.tsx
@@ -1,5 +1,18 @@
-const HomePage = () => {
-	return <div>Hello Store</div>;
+import getBillboard from '@/actions/get-billboard';
+import Billboard from '@/components/billboard';
+import Container from '@/components/ui/container';
+
+export const revalidate = 0;
+
+const HomePage = async () => {
+	const billboard = await getBillboard('311b23d1-be99-4be4-960d-7fcf65df331d');
+	return (
+		<Container>
+			<div className="space-y-10 pb-10">
+				<Billboard data={billboard} />
+			</div>
+		</Container>
+	);
 };
 
 export default HomePage;
