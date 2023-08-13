47cdf7fe350bc6aed07065559f9e801ef2986a81
tayoolaigbe <tayoolaigbe@outlook.com>
2023-08-13 22:32:08 +0100

    Added the Summary component in cart

--- ecommerce-store/components/navbar-actions.tsx
+++ ecommerce-store/components/navbar-actions.tsx
@@ -1,9 +1,10 @@
 'use client';
 
-import { useEffect, useState } from 'react';
-
 import { ShoppingBag } from 'lucide-react';
-import Button from '@/components/ui/Button';
+import { useEffect, useState } from 'react';
+import { useRouter } from 'next/navigation';
+
+import Button from '@/components/ui/button';
 import useCart from '@/hooks/use-cart';
 
 const NavbarActions = () => {
@@ -12,6 +13,8 @@ const NavbarActions = () => {
 	useEffect(() => {
 		setIsMounted(true);
 	}, []);
+
+	const router = useRouter();
 	const cart = useCart();
 
 	if (!isMounted) {
@@ -20,7 +23,10 @@ const NavbarActions = () => {
 
 	return (
 		<div className="ml-auto flex items-center gap-x-4">
-			<Button className="flex items-center rounded-full bg-black px-4 py-2">
+			<Button
+				className="flex items-center rounded-full bg-black px-4 py-2"
+				onClick={() => router.push('/cart')}
+			>
 				<ShoppingBag size={20} color="white" />
 				<span className="ml-2 text-sm font-medium text-white">
 					{cart.items.length}
